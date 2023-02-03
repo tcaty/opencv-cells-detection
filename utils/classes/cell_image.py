df58c1545b16b188f8aab39dafef98323a8c0b3f
tcaty <ltcatyl@gmail.com>
2023-02-03 10:21:40 +0300

    temp commit

--- utils/classes/cell_image.py
+++ utils/classes/cell_image.py
@@ -27,15 +27,40 @@ class CellImage(Image):
         return self._src
 
     def get_cells_using_sbd(self):
+        # image preprocessing
         self.filter(alg=FilterAlg.GAUSSIAN)
         self.detect_contours(alg=ContoursDetectingAlg.OTSU_BINARIZATION)
-        self.morph_transform(alg=MorphOperation.CLOSING)
+        self.morph_transform(alg=MorphOperation.OPENING)
+        self.morph_transform(alg=MorphOperation.CLOSING,
+                             kernel=np.ones((7, 7), np.uint8), iterations=2)
+
         contours, _ = cv.findContours(
             self.src, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        min_cell_area = 1000 # TODO: detect this one automaticaly
-        contours = tuple(
-            [contour for contour in contours if cv.contourArea(contour) > min_cell_area])
+        min_cell_area = 1000  # TODO: detect this one automaticaly
+        contours = [contour for contour in contours if cv.contourArea(
+            contour) > min_cell_area]
+
+        params = cv.SimpleBlobDetector_Params()
+        params.filterByInertia = True
+        params.filterByConvexity = True
+        params.filterByCircularity = True
+        params.filterByArea = False
+        params.minInertiaRatio = 0.01
+        params.maxInertiaRatio = 0.7
+        params.minConvexity = 0.8
+        params.minCircularity = 0.1
+
+        detector = cv.SimpleBlobDetector_create(params)
+        keypoints = detector.detect(self.src)
+        cells_contours = []
+        for keypoint in keypoints:
+            x, y = tuple(map(int, keypoint.pt))
+            for contour in contours:
+                if cv.pointPolygonTest(contour, (x, y), False) > 0:
+                    cells_contours.append(contour)
+
+        # noiseless mask
         noiseless_mask = np.zeros_like(self.src)
         cv.fillPoly(noiseless_mask, contours, 1)
         self._src = noiseless_mask
-        return noiseless_mask
+        return self.src
